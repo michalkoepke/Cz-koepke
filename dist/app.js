7255c021343e2c26faaef09be3ce56fc97f6045f
michalkoepke <michalkoepke@gmail.com>
2020-04-01 15:52:45 +0200

    czwarty commit - zrobiony responsive navbar, dziala dla telefonow i tabletow  z przelaczaniem ikony burger-close

--- dist/app.js
+++ dist/app.js
@@ -2,9 +2,31 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const test = document.querySelector('.nav-inside');
 
+
+
     burger.addEventListener('click', () => {
         test.classList.toggle('nav-inside-active');
+
+
+
+
     });
 }
 
+
+const iconToggle = () => {
+
+    const ikona = document.querySelector('.burger i');
+    console.log(ikona);
+
+    ikona.addEventListener('click', () => {
+
+        ikona.classList.toggle('fa-times');
+
+    });
+
+}
+
+
 navSlide();
+iconToggle();
